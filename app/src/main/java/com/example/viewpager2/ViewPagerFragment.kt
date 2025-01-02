07b1765eb2921fb1a488118bc88e93bb49e2c849
Aleksandr131087 <an.petrov13@gmail.com>
2025-01-02 16:10:19 +0400

    ДЗ ViewPager2Practice

--- app/src/main/java/com/example/viewpager2/ViewPagerFragment.kt
+++ app/src/main/java/com/example/viewpager2/ViewPagerFragment.kt
@@ -1,16 +1,18 @@
 package com.example.viewpager2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
 
 class ViewPagerFragment : Fragment() {
-
+    private var check = true
 
 
     override fun onCreateView(
@@ -22,13 +24,22 @@ class ViewPagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val picture_namePagerTV: TextView = view.findViewById(R.id.picture_namePagerTV)
         val autor_namePagerTV: TextView = view.findViewById(R.id.autor_namePagerTV)
         val imagePagerIV: ImageView = view.findViewById(R.id.imagePagerIV)
+        val startBTN: Button = view.findViewById(R.id.startBTN)
         val viewpagerItem = arguments?.getSerializable("vp") as ViewPagerModel
-        picture_namePagerTV.text = viewpagerItem.picture_name
-        autor_namePagerTV.text=viewpagerItem.author_name
-        imagePagerIV.setImageResource(viewpagerItem.imageView)
-    }
+        check = viewpagerItem.checkButton!!
 
-}
+        picture_namePagerTV.text = viewpagerItem.picture_name
+        autor_namePagerTV.text = viewpagerItem.author_name
+        imagePagerIV.setImageResource(viewpagerItem.imageView)
+
+        if (!check)
+            startBTN.visibility = View.VISIBLE
+        startBTN.setOnClickListener {
+            startActivity(Intent(activity, StartActivity::class.java))
+        }
+
+    }}
--- app/src/main/java/com/example/viewpager2/ViewPagerModel.kt
+++ app/src/main/java/com/example/viewpager2/ViewPagerModel.kt
@@ -5,21 +5,17 @@ import java.io.Serializable
 class ViewPagerModel (
     val picture_name: String,
     val author_name: String,
-    val imageView: Int
+    val imageView: Int,
+    var checkButton: Boolean?
+
 
 ) : Serializable{
     companion object {
 val viewPagerList = mutableListOf(
-    ViewPagerModel("Утро в сосновом лесу", "И.И. Шишкин", R.drawable.utro),
-    ViewPagerModel("Полевая рябинка", "А.А. Рылов", R.drawable.polevaya_rybina),
-ViewPagerModel("Девочка с персиками", "В.А. Серов", R.drawable.spersikamy),
-    ViewPagerModel("Мона лиза", "Л. Да Винчи", R.drawable.liza),
-    ViewPagerModel("9 Вал", "И.K. Айвазовский", R.drawable.val9),
-    ViewPagerModel("Последний день Помпеи", "К.П. Брюллов", R.drawable.pompey),
-    ViewPagerModel("Демон", "М.А. Врубель", R.drawable.demon),
-    ViewPagerModel("Богатыри", "В.А. Васнецов", R.drawable.bogatyr),
-    ViewPagerModel("Купание красного коня", "К.С. Петров-Водкин", R.drawable.konya),
-    ViewPagerModel("Подсолнухи", "Винсент Ван Гог", R.drawable.podsolnuhi),
+    ViewPagerModel("Банк Петрониус", "Самый надежный банк России", R.drawable.utro, true),
+    ViewPagerModel("Банк Петрониус", "25 лет вместе с Вами", R.drawable.polevaya_rybina, true),
+ViewPagerModel("Банк Петрониус", "Это Правильный Выбор", R.drawable.spersikamy, false),
+
 
     )
     }
